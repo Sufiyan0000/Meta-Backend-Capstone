db218ba3c4bdc102e0da70f8f4d0c201845e88eb
Sufiyan0000 <alimdsufiyan89@gmail.com>
2025-11-06 18:43:26 +0530

    Saved my local model changes

--- LittleLemon/my_app/models.py
+++ LittleLemon/my_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem,on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+
+    def __str__(self):
+        return f'Cart of {self.user.username} has {self.menu_item} in cart.'
